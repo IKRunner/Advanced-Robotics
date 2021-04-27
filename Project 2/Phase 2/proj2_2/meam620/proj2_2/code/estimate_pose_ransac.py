351da0da829eae0f89b934058ebab5854e2bc662
IKRunner <iachilihu@gmail.com>
2021-04-27 12:23:22 -0400

    Update

--- Project 2/Phase 2/proj2_2/meam620/proj2_2/code/estimate_pose_ransac.py
+++ Project 2/Phase 2/proj2_2/meam620/proj2_2/code/estimate_pose_ransac.py
@@ -35,9 +35,63 @@ def solve_w_t(uvd1, uvd2, R0):
     :param R0: Rotation type - base rotation estimate
     :return: w, t : 3x1 ndarray estimate for rotation vector, 3x1 ndarray estimate for translation
     """
+    '''
+    # initialize inliers to all false
+    best_inliers = np.zeros(n, dtype=bool)
+    n = uvd1.shape[1]
+    If ransanc_iterations < 1:
+
+
+    A -> 2n * 6
+    B -> 2n * 1
+    Y = R.as_matrix() @ np.vstack((uvd2[0:2, :], np.ones((1, n))))
+    Y1 = Y[0, :]
+
+
+    '''
 
     # TODO Your code here replace the dummy return value with a value you compute
-    w = t = np.zeros((3,1))
+    w = np.zeros((3,1))
+    t = np.zeros((3,1))
+
+    # Generate y matrix
+    _, n = uvd1.shape
+    Y = R0.as_matrix() @ np.vstack((uvd2[0:2, :], np.ones((1, n))))
+
+    # Loop through all correspondences
+    b = np.zeros((2 * n,1))
+    A = np.zeros((2 * n, 6))
+    for i in range(n):
+        # Generate b
+        u1_prime = uvd1[0, i]
+        v1_prime = uvd1[1, i]
+        b[i:2, :] = -np.hstack((np.eye(2,2), np.array([[-u1_prime], [-v1_prime]]))) @ Y[:, i]
+
+        # Generate A matrix
+        A[i:2, :] =
+
+
+
+        # Generate y {3x3 * 3x1 = 3x1}
+        # u1_prime = uvd1[0, i]
+        # v1_prime = uvd1[1, i]
+        # u2_prime = uvd2[0, i]
+        # v2_prime = uvd2[1, i]
+        # y = (R0 @ np.array([[u2_prime], [u2_prime], [1]]))[:,0]
+        #
+        # # Generate b {2x3 * 3x1 = 2x1}
+        # x = np.array([[1, 0, -u1_prime], [0, 1, -v1_prime]])
+        # b = -x @ y
+        #
+        # # Generate A {2x6}
+        # A = x @ np.array([[0, y[2], -y[1], uvd2[2, i], 0, 0],
+        #               [-y[2], 0, y[0], 0, uvd2[2, i], 0],
+        #               [y[1], -y[0], 0, 0, 0, uvd2[2, i]]])
+        #
+        # # Solve system
+        # np.linalg.solve(A, b)
+
+
     return w, t
 
 
